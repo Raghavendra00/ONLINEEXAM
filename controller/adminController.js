4b0d0e683dedfa18f2ddfce29f56d863082023d2
Raghavendra kumar <raghavendrakumar217@gmail.com>
2021-11-10 11:22:55 +0530

    bcrypt,answers

--- controller/adminController.js
+++ controller/adminController.js
@@ -2,8 +2,8 @@ const User = require('../models/user')
 
 exports.viewstudents = async (req, res) => {
 
-    const students = await User.find()
+    const students = await User.find({isAdmin:false})
 
-    // console.log(students);
-    res.render('studentlist',{students})
+    console.log(students);
+    res.render('studentlist',{students, isAdmin:req.session.user.isAdmin,email:req.session.user.email})
 }
--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,8 @@
 const user = require('../models/user')
 // const mongoose=require('../models/user')
 
+const bcrypt = require('bcrypt')
+
 
 
 exports.home = (req, res) => {
@@ -9,7 +11,7 @@ exports.home = (req, res) => {
 
 exports.dashboard = (req, res) => {
     // console.log(req.session.user);
-    res.render('dashboard')
+    res.render('dashboard', {isAdmin:req.session.user.isAdmin,email:req.session.user.email})
 }
 
 exports.signup = (req, res) => {
@@ -22,6 +24,7 @@ exports.logout = (req, res) => {
         res.redirect('/')
      })
 }
+
 // exports.forgotpassword = (req, res) => {
 //     res.render('forgotpassword')
 // }
@@ -39,13 +42,15 @@ exports.savedata = async (req, res) => {
           return res.render("signup", { msg: "PhoneNumber already exists" });
         }
 
+        const hashedPassword = await bcrypt.hashSync(req.body.password,12)
+
         var data = {
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone,
-            password: req.body.password,
-            isAdmin:false,
-            examTaken:false
+          name: req.body.name,
+          email: req.body.email,
+          phone: req.body.phone,
+          password: hashedPassword,
+          isAdmin: false,
+          examTaken: false,
         };
 
         const result = await user(data).save();
@@ -58,10 +63,19 @@ exports.savedata = async (req, res) => {
 }
 
 exports.postsignin = async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
 
-    const result = await user.find({ email: req.body.email, password: req.body.password })
+    const result = await user.find({ email: req.body.email })
+    console.log(result);
+    
+
     if (result.length > 0) {
+
+        const isValid = await bcrypt.compareSync(req.body.password,result[0].password)
+
+        if (!isValid) {
+            return res.render("signin", { msg: "Incorrect Details" });
+        }
         req.session.user = result[0]
         req.session.save(() => {
             return res.redirect('/dashboard')
@@ -75,6 +89,10 @@ exports.postsignin = async (req, res) => {
 
 exports.exam = async (req, res) => {
 
+    if (req.session.user.isAdmin == true) {
+        return res.redirect("/dashboard");
+    }
+
     // console.log(req.session.user);
 
     var userid = req.session.user._id
@@ -83,24 +101,54 @@ exports.exam = async (req, res) => {
 
     if (!result.examTaken) {
         
-        return res.render("exam");
+        return res.render("exam", {
+          isAdmin: req.session.user.isAdmin,
+          email: req.session.user.email,
+        });
     }
 
-    res.render('alreadytaken')
+    res.render("alreadytaken", {
+      isAdmin: req.session.user.isAdmin,
+      email: req.session.user.email,
+    });
 };
 
 exports.sendans = async (req, res) => {
 
+    const answers = {
+      Q1: "a",
+      Q2: "b",
+      Q3: "b",
+      Q4: "c",
+      Q5: "a",
+      Q6: "a",
+      Q7: "c",
+      Q8: "b",
+    };
+
+    const userAnswers = req.body
+    var total = 0
+    for (var i in answers) {
+        if (answers[i] == userAnswers[i]) {
+            total+=1
+        }
+    }
+
+    // console.log(total);
+
     var userid = req.session.user._id
 
-    const result = await user.findByIdAndUpdate(userid,{examTaken:true})
+    const result = await user.findByIdAndUpdate(userid,{examTaken:true,score:total})
     // req.session.user.examTaken = 
-    console.log(req.body);
+    // console.log(req.body);
     res.redirect('/thankyou')
 };
 
 exports.thankyou = (req, res) => {
 
     // console.log(req.body);
-    res.render('thankyou')
+    res.render("thankyou", {
+      isAdmin: req.session.user.isAdmin,
+      email: req.session.user.email,
+    });
 };
